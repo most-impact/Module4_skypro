6bfd0c0280c49baf623e2543d8859c3e1608042e
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2024-10-10 22:44:27 +0300

    completed tasks

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,39 +2,53 @@ from typing import Any
 
 import pytest
 
-from src.main import Category, Product
+from src.main import Category, Product, Smartphone, LawnGrass
 
 
 @pytest.fixture()
-def product_iphone() -> Any:
-    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+def lawn_grass1() -> Any:
+    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
 
 
 @pytest.fixture()
-def product_samsung() -> Any:
-    return Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-    )
+def smartphone_iphone() -> Any:
+    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
 
 
 @pytest.fixture()
-def product_xiaomi() -> Any:
-    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+def smartphone_samsung() -> Any:
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
 
 
 @pytest.fixture()
-def category_phone(product_iphone: Any, product_samsung: Any) -> Any:
+def smartphone_xiaomi() -> Any:
+    return Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+
+
+@pytest.fixture()
+def category_phone(smartphone_iphone: Any, smartphone_samsung: Any) -> Any:
     return Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product_iphone, product_samsung],
+        [smartphone_iphone, smartphone_samsung],
     )
 
 
-def test_product(product_iphone: Any, product_samsung: Any) -> Any:
-    assert product_iphone.name == "Iphone 15"
-    assert product_iphone.description == "512GB, Gray space"
-    assert product_iphone.quantity == 8
+def test_smartphone(smartphone_iphone: Any) -> Any:
+    assert smartphone_iphone.memory == 512
+    assert smartphone_iphone.model == "15"
+
+
+def test_lawn_grass(lawn_grass1: Any) -> Any:
+    assert lawn_grass1.name == "Газонная трава"
+    assert lawn_grass1.price == 500.0
+
+
+def test_product(smartphone_iphone: Any) -> Any:
+    assert smartphone_iphone.name == "Iphone 15"
+    assert smartphone_iphone.description == "512GB, Gray space"
+    assert smartphone_iphone.quantity == 8
 
     new_product = Product.new_product(
         {
@@ -49,31 +63,31 @@ def test_product(product_iphone: Any, product_samsung: Any) -> Any:
     assert new_product.price == "Цена не должна быть нулевая или отрицательная"
 
 
-def test_category(category_phone: Any, product_xiaomi: Any) -> Any:
+def test_category(category_phone: Any, smartphone_xiaomi: Any) -> Any:
     assert category_phone.name == "Смартфоны"
     assert (
-        category_phone.description
-        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+            category_phone.description
+            == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
     assert category_phone.category_count == 1
     assert category_phone.product_count == 2
 
     # testing the add_product function
-    category_phone.add_product(product_xiaomi)
+    category_phone.add_product(smartphone_xiaomi)
 
     assert category_phone.product_count == 3
     assert category_phone.products == (
-        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
-        + "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
-        + "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+            "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+            + "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+            + "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
     )
 
 
-def test_magic_methods_product(product_iphone: Any, product_samsung: Any) -> Any:
-    assert str(product_iphone) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
-    assert str(product_samsung) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+def test_magic_methods_product(smartphone_iphone: Any, smartphone_samsung: Any) -> Any:
+    assert str(smartphone_iphone) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
+    assert str(smartphone_samsung) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
 
-    assert product_iphone + product_samsung == 2580000.0
+    assert smartphone_iphone + smartphone_samsung == 2580000.0
 
 
 def test_magic_methods_category(category_phone: Any) -> Any:
