d5ed5d8f35710aaec5de7485805bc270a5be252e
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2024-10-12 11:48:50 +0300

    completed tasks

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,28 +2,55 @@ from typing import Any
 
 import pytest
 
-from src.main import Category, Product, Smartphone, LawnGrass
+from src.main import Category, LawnGrass, Product, Smartphone
 
 
 @pytest.fixture()
 def lawn_grass1() -> Any:
-    return LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    return LawnGrass(
+        "Газонная трава",
+        "Элитная трава для газона",
+        500.0,
+        20,
+        "Россия",
+        "7 дней",
+        "Зеленый",
+    )
 
 
 @pytest.fixture()
 def smartphone_iphone() -> Any:
-    return Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    return Smartphone(
+        "Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space"
+    )
 
 
 @pytest.fixture()
 def smartphone_samsung() -> Any:
     return Smartphone(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый")
+        "Samsung Galaxy S23 Ultra",
+        "256GB, Серый цвет, 200MP камера",
+        180000.0,
+        5,
+        95.5,
+        "S23 Ultra",
+        256,
+        "Серый",
+    )
 
 
 @pytest.fixture()
 def smartphone_xiaomi() -> Any:
-    return Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+    return Smartphone(
+        "Xiaomi Redmi Note 11",
+        "1024GB, Синий",
+        31000.0,
+        14,
+        90.3,
+        "Note 11",
+        1024,
+        "Синий",
+    )
 
 
 @pytest.fixture()
@@ -33,6 +60,15 @@ def category_phone(smartphone_iphone: Any, smartphone_samsung: Any) -> Any:
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [smartphone_iphone, smartphone_samsung],
     )
+
+
+def test_mixin_log(capfd) -> Any:
+    product = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+
+    expected_repr = f"Product({product.__dict__})"
+    assert repr(product) == expected_repr
 
 
 def test_smartphone(smartphone_iphone: Any) -> Any:
@@ -66,8 +102,8 @@ def test_product(smartphone_iphone: Any) -> Any:
 def test_category(category_phone: Any, smartphone_xiaomi: Any) -> Any:
     assert category_phone.name == "Смартфоны"
     assert (
-            category_phone.description
-            == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+        category_phone.description
+        == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
     assert category_phone.category_count == 1
     assert category_phone.product_count == 2
@@ -77,15 +113,18 @@ def test_category(category_phone: Any, smartphone_xiaomi: Any) -> Any:
 
     assert category_phone.product_count == 3
     assert category_phone.products == (
-            "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
-            + "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
-            + "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+        + "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        + "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
     )
 
 
 def test_magic_methods_product(smartphone_iphone: Any, smartphone_samsung: Any) -> Any:
     assert str(smartphone_iphone) == "Iphone 15, 210000.0 руб. Остаток: 8 шт."
-    assert str(smartphone_samsung) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    assert (
+        str(smartphone_samsung)
+        == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+    )
 
     assert smartphone_iphone + smartphone_samsung == 2580000.0
 
